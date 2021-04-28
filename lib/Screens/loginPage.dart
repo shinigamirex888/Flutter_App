1ec0ccfd1ed05a987cb05ec49877547a55da9b43
Snehil <snehilsneh888@gmail.com>
2021-04-28 16:54:18 +0530

    bug fixes

--- lib/Screens/loginPage.dart
+++ lib/Screens/loginPage.dart
@@ -18,7 +18,7 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             children: [
               Image.asset(
-                "assets\images\login.png",
+                "assets/images/login.png",
                 fit: BoxFit.cover,
               ),
               SizedBox(
